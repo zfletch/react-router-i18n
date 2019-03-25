41a67601e122af79fe70e9a04140effaa5012207
Aonkon Dey <deyuttsha@gmail.com>
2019-03-25 15:04:28 -0400

    Adding Jest test to components in lib

---
+++ src/lib/components/I18n/withLocale.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import { MemoryRouter } from 'react-router-dom';
+import withLocale from './withLocale';
+
+it('renders without crashing', () => {
+	const OriginalComponent=  () => (<div/>);
+	const WrappedComponent= withLocale(OriginalComponent);
+	const div = window.document.createElement('div');
+	const component = (
+    <MemoryRouter>
+      <WrappedComponent />
+    </MemoryRouter>
+    );
+	render(component, div);
+});
