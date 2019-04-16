5edf45fa7c2b9bba949aef7922b3c248d23db998
zfletch <zfletch2@gmail.com>
2019-04-16 15:16:22 -0400

    minor fixes
    * Fix formatting
    * Fix lint errors
    * Update PropType to accept `node` instead of `element`

--- src/demo/App.test.js
+++ src/demo/App.test.js
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import App from './App';
 
 it('renders with full navbar and Home page', () => {
-  	const tree = renderer.create(<App />).toJSON();
-  	expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
