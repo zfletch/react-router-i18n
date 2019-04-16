8cb818d3e6759df75ef984af3f63738b2bcd0ca2
zfletch <zfletch2@gmail.com>
2019-04-16 15:31:21 -0400

    Updates

---
+++ precache-manifest.1b68d8ff55b4aeb66bf1eb5a433cd412.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a0448a5a21cb864e49",
+    "url": "/react-router-i18n/static/js/main.22a0448a.chunk.js"
+  },
+  {
+    "revision": "072efb29641302b1a71b",
+    "url": "/react-router-i18n/static/js/runtime~main.072efb29.js"
+  },
+  {
+    "revision": "279f15fd5a7792cbebb5",
+    "url": "/react-router-i18n/static/js/2.279f15fd.chunk.js"
+  },
+  {
+    "revision": "d3ee49d2e660a03e892d90d6440cfe48",
+    "url": "/react-router-i18n/index.html"
+  }
+];
