f741d0e979fa359761565b0f3d421a9519693968
zfletch <zfletch2@gmail.com>
2019-04-19 11:36:39 -0400

    detailed tests

--- src/lib/components/I18n/createI18n.test.js
+++ src/lib/components/I18n/createI18n.test.js
@@ -1,27 +1,130 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 import createI18n from './createI18n';
 
-it('renders without crashing', () => {
-  const locales = ['en', 'fr'];
-  const translations = {
-    en: {
-      home: 'Home',
-    },
-    fr: {
-      home: 'Accueil',
-    },
-  };
-  const I18n = createI18n(
+const locales = ['en', 'fr'];
+let translations = {
+  en: {
+    home: 'Home',
+  },
+  fr: {
+    home: 'Accueil',
+  },
+};
+
+let I18n;
+beforeEach(() => {
+  I18n = createI18n(
     locales,
     translations,
   );
+});
+
+it('renders without crashing', () => {
   const component = (
     <MemoryRouter>
       <I18n t="home" />
     </MemoryRouter>
   );
-  const div = window.document.createElement('div');
-  render(component, div);
+
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('shows the translation of another locale', () => {
+  const component = (
+    <MemoryRouter initialEntries={['/fr/']}>
+      <I18n t="home" />
+    </MemoryRouter>
+  );
+
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+describe('nested translations', () => {
+  beforeAll(() => {
+    translations = {
+      en: {
+        nested: {
+          translation: 'Nested',
+        },
+      },
+    };
+  });
+
+  it('shows a nested translation', () => {
+    const component = (
+      <MemoryRouter>
+        <I18n t="nested.translation" />
+      </MemoryRouter>
+    );
+
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('default translations', () => {
+  beforeAll(() => {
+    translations = {
+      en: {
+        home: 'Home',
+      },
+    };
+  });
+
+  it('uses the default language translation when there is none', () => {
+    const component = (
+      <MemoryRouter initialEntries={['/fr/']}>
+        <I18n t="home" />
+      </MemoryRouter>
+    );
+
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('uses props.children when there is no translation', () => {
+    const component = (
+      <MemoryRouter>
+        <I18n t="other">
+          Other
+        </I18n>
+      </MemoryRouter>
+    );
+
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it('does not render when there is no translation', () => {
+    const component = (
+      <MemoryRouter>
+        <I18n t="other" />
+      </MemoryRouter>
+    );
+
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('uses the default if one is given and there is no translation', () => {
+    I18n = createI18n(
+      locales,
+      translations,
+      'Missing',
+    );
+
+    const component = (
+      <MemoryRouter>
+        <I18n t="other" />
+      </MemoryRouter>
+    );
+
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
--- src/lib/components/I18n/withLocale.test.js
+++ src/lib/components/I18n/withLocale.test.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import { render } from 'react-testing-library';
-import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { MemoryRouter, Route } from 'react-router-dom';
 import withLocale from './withLocale';
 
+const OriginalComponent = ({ to, text }) => (
+  <div>
+    {to}
+    {text}
+  </div>
+);
+const WrappedComponent = withLocale(OriginalComponent);
+
 it('renders without crashing', () => {
-  const OriginalComponent = () => <div />;
-  const WrappedComponent = withLocale(OriginalComponent);
-  const div = window.document.createElement('div');
   const component = (
     <MemoryRouter>
       <WrappedComponent to="test" />
     </MemoryRouter>
   );
 
-  render(component, div);
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders normally when there is no match', () => {
+  const component = (
+    <MemoryRouter initialEntries={['/home/']}>
+      <Route exact path="/:locale?/home" component={() => <WrappedComponent to="test" />} />
+    </MemoryRouter>
+  );
+
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('adds the locale to `to` when there is a match', () => {
+  const component = (
+    <MemoryRouter initialEntries={['/fr/home/']}>
+      <Route exact path="/:locale?/home" component={() => <WrappedComponent to="test" />} />
+    </MemoryRouter>
+  );
+
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('passes through any other arguments', () => {
+  const component = (
+    <MemoryRouter>
+      <WrappedComponent to="test" text="example" />
+    </MemoryRouter>
+  );
+
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
 });
