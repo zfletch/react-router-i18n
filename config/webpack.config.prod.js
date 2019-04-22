9bb9d3d52f0c9d08e11de98594cf2c774213f625
zfletch <zfletch2@gmail.com>
2019-04-22 10:18:04 -0400

    tweak scripts for publication

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -481,6 +481,7 @@ module.exports = {
   externals: {
     react: 'react',
     'react-dom': 'react-dom',
+    'react-router-dom': 'react-router-dom',
   },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
